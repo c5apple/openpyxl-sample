c344c08e5204964dd98e5be153476898a2a7f28c
c5apple <c5apple_party@yahoo.co.jp>
2020-03-06 16:19:20 +0900

    tutorial

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 
 from openpyxl import Workbook
 
-# ワークブック取得
+# 新規ワークブック作成
 wb = Workbook()
 
 # アクティブワークシート取得
@@ -10,7 +10,10 @@ ws = wb.active
 
 # 値を入力する
 ws['A1'] = 42
-ws['B1'] = 'こんにちは'
+ws.cell(row=1, column=2, value='こんにちは') # B1
+
+for c in ws['A2:C3']:
+    c = 'まとめて'
 
 # まとめて最終行に入力する
 ws.append([1, 2, 3])
@@ -19,5 +22,18 @@ ws.append([1, 2, 3])
 import datetime
 ws['A2'] = datetime.datetime.now()
 
-# Save the file
+# 新規ワークシートを作成する
+ws1 = wb.create_sheet('NewSheet', 1)
+
+ws2 = wb.create_sheet('NewSheet2', 0)
+ws2.title = 'NewSheet2!!' # ValueError: Worksheet titles must be unicode
+ws2.sheet_properties.tabColor = '1072BA'
+ws2['A1'] = 'A1'
+
+ws3 = wb.copy_worksheet(ws2)
+
+ws4 = wb['NewSheet']
+ws4['A1'] = 'これは左から3番目にあるシート'
+
+# ファイル保存
 wb.save("sample.xlsx")
